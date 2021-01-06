ecf5b50e723a3106ac662f1f5835bb9fd48850fe
JIA-HONG-CHU <56266563+JIA-HONG-CHU@users.noreply.github.com>
2021-01-06 14:47:24 +0800

    Update Unb_training_model_and_evl_table.R

--- code/Unb_training_model_and_evl_table.R
+++ code/Unb_training_model_and_evl_table.R
@@ -20,22 +20,22 @@ if (!dir.exists(output_dir)){
 
 p <- arg_parser("Unbalanced data training model use in balanced testing data")
 p <- add_argument(p, "--input", help="unbalanced data csv file",default = "./data/fake_job_postings_TFIDF.csv")
-p <- add_argument(p, "--model_weight", help="training model weight",default = "./model_results/unb/gbm_ub.rds")
+p <- add_argument(p, "--training_rds", help="training model weight",default = "./model_results/unb/gbm_ub.rds")
 p <- add_argument(p, "--output", help="evaluation table",default = "./model_results/unb/cnf_gbm_unb.csv")
 #  trailingOnly 如果是TRUE的話，會只編輯command-line出現args的值args <- 
 args <- parse_args(p, commandArgs(trailingOnly = TRUE))
-df1<-read.csv(args$input_csv,fileEncoding='utf-8')
+df1<-read.csv(args$input,fileEncoding='utf-8')
 set.seed(20201219)
 index <-  sort(sample(nrow(df1), nrow(df1)*.75))
 train <- df1[index,]
 test <-  df1[-index,]
-#train.gbm_ub <- gbm(train$fraudulent ~ .,
-                    #data=train, distribution="bernoulli",
-                    #n.trees=1000,interaction.depth=4,
-                    #shrinkage=0.05,verbose = T)
-#saveRDS(train.gbm_ub,args$model_weight)
+train.gbm_ub <- gbm(train$fraudulent ~ .,
+                    data=train, distribution="bernoulli",
+                    n.trees=1000,interaction.depth=4,
+                    shrinkage=0.05,verbose = T)
+saveRDS(train.gbm_ub,args$training_rds)
 
-my_model_gbm <- readRDS(args$model_weight)
+my_model_gbm <- readRDS(args$training_rds)
 pred_gbm <- predict(object=my_model_gbm,newdata=test,type="response")
 predbin_gbm <- as.factor(ifelse(pred_gbm>0.5,1,0))
 k <- as.factor(test$fraudulent)
@@ -53,7 +53,7 @@ cgbm <- data.frame("ub_model_test_ub",round(acc.confusematrix_gbm,2),round(sens.
                    round(prec.confusematrix_gbm,2),round(rec.confusematrix_gbm,2),round(f1.confusematrix_gbm,2),
                    round(balacc.confusematrix_gbm,2),round(auc.gbm,2))
 names(cgbm) <- c("performance","accuracy","sensitivity","specificity","precision","recall","F1-score","balanced_accuaracy","auc")
-write.csv(cgbm,file=args$output_csv)
+write.csv(cgbm,file=args$output)
 png(filename=paste("gbm_unb",".png",sep=""))
 plot(roc.gbm, YIndex = F, values = F)
 dev.off()
