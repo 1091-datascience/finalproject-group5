b1bd6c28f45bb943d4e14f11e093213c2c5caae6
JIA-HONG-CHU <56266563+JIA-HONG-CHU@users.noreply.github.com>
2021-01-06 14:43:55 +0800

    Update Unbalanced_data2Balanced_data.R

--- code/Unbalanced_data2Balanced_data.R
+++ code/Unbalanced_data2Balanced_data.R
@@ -7,7 +7,7 @@ p <- add_argument(p, "--output", help="balanced data csv file",default = "./data
 # trailingOnly 如果是TRUE的話，會只編輯command-line出現args的值args <- 
 args <- parse_args(p, commandArgs(trailingOnly = TRUE))
 
-df1<-read.csv(args$input_csv,fileEncoding='utf-8')
+df1<-read.csv(args$input,fileEncoding='utf-8')
 y <- as.factor(df1$fraudulent)
 x <- df1[,c(-1,-5)]
 # (Synthetic Minority Oversampling Technique) 
@@ -15,4 +15,4 @@ data_osp <- ubBalance(X=x, Y=y, type="ubSMOTE", percOver=300, percUnder=150, ver
 balancedData_x<-data_osp$X
 fraudulent <- data_osp$Y
 balanceData <- cbind(balancedData_x,fraudulent)
-write.csv(balanceData,file=args$output_csv)
+write.csv(balanceData,file=args$output)
